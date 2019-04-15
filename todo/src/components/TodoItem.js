dff4d9ce353a6ebda85191498816f90cd947da38
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-04-14 23:28:18 -0400

    Creating branch @elvisd

--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -4,23 +4,21 @@ import { connect } from "react-redux";
 
 const TodoItem = props => {
   return (
-    <div className="card-panel">
-      <div className="row">
-        <div className="col s10">
-          <ul>
-            <li
-              onClick={() => props.markDone(props.id)}
-              style={{
-                textDecoration: props.completed ? `line-through` : `none`
-              }}
-            >
-              {props.task}
-            </li>
-          </ul>
-        </div>
-        <div className="col 2" onClick={() => props.deleteTodo(props.id)}>
-          <i className="material-icons">delete</i>
-        </div>
+    <div className="row">
+      <div className="col s10">
+        <ul>
+          <li
+            onClick={() => props.markDone(props.id)}
+            style={{
+              textDecoration: props.completed ? `line-through` : `none`
+            }}
+          >
+            {props.task}
+          </li>
+        </ul>
+      </div>
+      <div className="col 2" onClick={() => props.deleteTodo(props.id)}>
+        <i className="material-icons">delete</i>
       </div>
     </div>
   );
